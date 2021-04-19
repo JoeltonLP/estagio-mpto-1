7627e7d5b21bfa542cfa9f5fc6165d9cbea01815
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2021-04-19 15:06:28 -0300

    foi iniciada a implementação para o endpoint que lista todos os estados
    related #6

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+
+class State(models.Model):
+    name = models.CharField('Nome', max_length=100, unique=True)
+    abbreviation = models.CharField('Sigla', max_length=2, unique=True)
