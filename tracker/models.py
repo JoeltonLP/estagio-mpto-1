5ca2d62e81d565a19bd0ba24c2d9a7af55e20fcf
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2021-04-19 15:38:01 -0300

    foi finalziada a implemtação da lsita de estados, assim como a implementação da criação de novos estados
    closes #1
    closes #6

--- tracker/models.py
+++ tracker/models.py
@@ -5,3 +5,6 @@ from django.db.models.fields import CharField
 class State(models.Model):
     name = models.CharField('Nome', max_length=100, unique=True)
     abbreviation = models.CharField('Sigla', max_length=2, unique=True)
+
+    def __str__(self) -> str:
+        return f'{self.name}/{self.abbreviation}'
