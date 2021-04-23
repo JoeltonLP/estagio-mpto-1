d64f8663f242a2a939506867e3c938a51fed6daa
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2021-04-23 15:10:16 -0300

    foi implemntado o recurso natual person
    closes #24

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,7 +1,37 @@
 from helpers.serializer import BaseSerializer
-from .models import State, City
+from .models import State, City, Person, NaturalPerson
 
 
+class PersonSerializer(BaseSerializer):
+    
+    _model = Person
+    
+    @classmethod
+    def encode(cls, instance):
+        result = super().encode(instance)
+
+        result.update(
+            name=instance.name
+        )
+
+        return result
+
+
+class NaturalPersonSerializer(PersonSerializer):
+    
+    _model = NaturalPerson
+    
+    @classmethod
+    def encode(cls, instance):
+        result = super().encode(instance)
+
+        result.update(
+            cpf=instance.cpf
+        )
+
+        return result
+
+    
 
 
 class StateSerializer(BaseSerializer):
